5f2ef144b9b0e7ee7dc0b9937b68f5b093f89913
komatsu <koma4business@gmail.com>
2017-11-14 10:25:07 +0900

    first commit

---
+++ get_robo_actdata_comment.py
@@ -0,0 +1,10 @@
+#csvファイルのテーブルを開いて、通し番号に対応するデータを持ってくる
+class GetRobotActionDataOfComment:
+
+        def __init__(self):
+                robot_action_data_of_comment = ""
+
+        def getRobotComment(self,comment_tbl_no):
+		#ここで、csvなどのテーブルファイルを開き、comment_tbl_noに対応するコメントを返り値の変数に入れる
+
+                return self.robot_action_data_of_comment

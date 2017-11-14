a435aeec5b6d832dcbf9d67387373264c337a31a
komatsu <koma4business@gmail.com>
2017-11-14 10:25:52 +0900

    first commit

---
+++ get_robo_actdata_motion.py
@@ -0,0 +1,10 @@
+#csvファイルのテーブルを開いて、通し番号に対応するデータを持ってくる
+class GetRobotActionDataOfMotion:
+
+        def __init__(self):
+                robot_action_data_of_motion = (0,0)
+
+        def getRobotMotion(self,motion_tbl_no):
+		#ここで、csvなどのテーブルファイルを開き、motion_tbl_noに対応するコメントを返り値の変数に入れる
+
+                return self.robot_action_data_of_motion

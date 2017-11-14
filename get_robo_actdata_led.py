9ec561876aeaa3add9b05feefbbdfa2d3a2f5c3d
tsuruhiro0805 <tsuruhiro0805@gmail.com>
2017-11-14 14:38:23 +0900

    LED制御追加
    テーブルデータをリストに格納

--- get_robo_actdata_led.py
+++ get_robo_actdata_led.py
@@ -1,12 +1,10 @@
-
+import tbl_robo_led
 
 #csvファイルのテーブルを開いて、通し番号に対応するデータを持ってくる
 class GetRobotActionDataOfLed:
-
         def __init__(self):
-                robot_action_data_of_Led = (0,0,0,0,0,0,0,0,0,0)
-
+                #robot_action_data_of_Led = (0,0,0,0,0,0,0,0,0,0)
+                self.robot_action_data_of_Led = tbl_robo_led.tblRobotLED()
         def getRobotLed(self,led_tbl_no):
 		#ここで、csvなどのテーブルファイルを開き、led_tbl_noに対応するコメントを返り値の変数に入れる
-
-                return self.robot_action_data_of_led
+                return self.robot_action_data_of_Led.DATA[led_tbl_no]

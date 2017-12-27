3ffbe275ea9c35a237ed2d7011a9081281ebd0c3
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2017-12-27 17:05:07 +0900

    changed from list to numpy array (may be changed)

--- tbl_tat_comment.py
+++ tbl_tat_comment.py
@@ -2,10 +2,11 @@
 #import tbl_robo_comment
 #for tat system
 #correspondence table
+import numpy as np
 
 class tblRoboComeTAT:
     def __init__(self):
-        self.DATA = [
+        self.DATA = np.array([
                 [0,30101],
                 [1,30102],
                 [2,30103],
@@ -16,4 +17,4 @@ class tblRoboComeTAT:
                 [7,30108],
                 [8,30109],
                 [9,30110],
-                [10,30111]]
+                [10,30111]])

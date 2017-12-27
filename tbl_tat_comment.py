ea94804869680ea8af23f57182e4939853548c8a
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2017-12-27 09:23:04 +0900

    add function to convert comment num from jbc sys to tat sys

---
+++ tbl_tat_comment.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#import tbl_robo_comment
+#for tat system
+#correspondence table
+
+class tblRoboComeTAT:
+    def __init__(self):
+        self.DATA = [
+                [0,30101],
+                [1,30102],
+                [2,30103],
+                [3,30104],
+                [4,30105],
+                [5,30106],
+                [6,30107],
+                [7,30108],
+                [8,30109],
+                [9,30110],
+                [10,30111]]
